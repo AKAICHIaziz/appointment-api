2c3127c93cbd9073d408ac06121f5abefdb6e1e9
Med Aziz Akaichi <akaichiaziz10@gmail.com>
2025-01-20 22:23:17 +0100

    adding the schedule for doctor

---
+++ src/main/java/com/azizdev/appointment_api/dto/DoctorScheduleDTO.java
@@ -0,0 +1,41 @@
+package com.azizdev.appointment_api.dto;
+
+public class DoctorScheduleDTO {
+
+    private String dayOfWeek;
+    private String startTime;
+    private String endTime;
+
+    public DoctorScheduleDTO(String dayOfWeek, String startTime, String endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public DoctorScheduleDTO() {
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
--- src/main/java/com/azizdev/appointment_api/model/DoctorSchedule.java
+++ src/main/java/com/azizdev/appointment_api/model/DoctorSchedule.java
@@ -1,6 +1,7 @@
 package com.azizdev.appointment_api.model;
 
 import com.azizdev.appointment_api.enums.DayOfWeekEnum;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
 import jakarta.persistence.*;
 
 
@@ -22,4 +23,52 @@ public class DoctorSchedule {
     private String startTime;
     private String endTime;
 
+    public DoctorSchedule(DayOfWeekEnum dayOfWeek, String startTime, String endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public DoctorSchedule() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public DayOfWeekEnum getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeekEnum dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
 }
